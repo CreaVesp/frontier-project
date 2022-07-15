6a02bc8bf7508526cf28ea56bb8677ea24e03929
Mikhail <creavesp@gmail.com>
2022-07-15 16:22:26 +0600

    Fighting with form functionality

--- src/components/UI/AddClientsForm.js
+++ src/components/UI/AddClientsForm.js
@@ -1,9 +1,12 @@
-import { useRef } from 'react';
+import { Fragment, useRef, useState } from 'react';
 import Modal from './Modal';
 
 import './AddForm.css';
 
 const AddClientsForm = props => {
+  const [userIsChecked, setUserIsChecked] = useState([]);
+  const [productIsChecked, setProductIsChecked] = useState([]);
+
   const nameRef = useRef('');
   const usersRef = useRef([]);
   const productsRef = useRef([]);
@@ -11,41 +14,73 @@ const AddClientsForm = props => {
   const users = props.users;
   const products = props.products;
 
-  const usersSelector = users.map(user => (
-    <div className='checkbox'>
+  // ⬇ Handlers
+  const userCheckedHandler = e => {
+    console.log(e.target.value);
+    let updatedList = [...userIsChecked];
+    if (e.target.checked) {
+      updatedList = [...userIsChecked, e.target.value];
+    } else {
+      updatedList.splice(userIsChecked.indexOf(e.target.value), 1);
+    }
+    // setUserIsChecked(updatedList);
+    setUserIsChecked(prevState => prevState.push(...updatedList));
+    console.log(userIsChecked);
+  };
+
+  const productCheckedHandler = e => {
+    let updatedList = [...productIsChecked];
+    if (e.target.productIsChecked) {
+      updatedList = [...productIsChecked, e.target.value];
+    } else {
+      updatedList.splice(productIsChecked.indexOf(e.target.value), 1);
+    }
+    setProductIsChecked(prevState => prevState.concat(updatedList));
+    console.log(productIsChecked);
+  };
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    const newClient = {
+      name: nameRef,
+      linkedUsers: usersRef,
+      availableProducts: productsRef,
+      id: `c${Math.trunc(Math.random() * 1000)}`,
+    };
+    console.log(newClient);
+    console.log('some shit is definetly going on');
+  };
+
+  // ⬇ Lists of checkboxes
+  const usersSelector = users.map((user, index) => (
+    <div ref={usersRef} className='checkbox' key={index}>
       <input
-        ref={usersRef}
         type='checkbox'
         id='user'
         name='user'
         value={user.id}
+        onChange={userCheckedHandler}
       />
       <label htmlFor='user'>{user.name}</label>
     </div>
   ));
 
-  const productsSelector = products.map(product => (
-    <div className='checkbox'>
+  const productsSelector = products.map((product, index) => (
+    <div ref={productsRef} className='checkbox' key={index}>
       <input
-        ref={productsRef}
         type='checkbox'
         id='product'
         name='product'
         value={product.id}
+        onChange={productCheckedHandler}
       />
       <label htmlFor='product'>{product.name}</label>
     </div>
   ));
 
-  const onSubmitHandler = e => {
-    e.preventDefault();
-    console.log(nameRef, usersRef, productsRef);
-    console.log('some shit is definetly going on');
-  };
-
   return (
     <Modal onHideModal={props.onHideModal}>
-      <div>
+      <Fragment>
         <form onSubmit={onSubmitHandler} className='form'>
           <div className='input'>
             <label htmlFor='name' className='description'>
@@ -74,7 +109,7 @@ const AddClientsForm = props => {
             Отмена
           </button>
         </div>
-      </div>
+      </Fragment>
     </Modal>
   );
 };
